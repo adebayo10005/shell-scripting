f13a5a10089204b29c04661c60a21b447d582a11
BOKO <adebayo10005@gmail.com>
2021-10-23 10:57:00 -0700

    Added topics in O4-variable.sh

---
+++ 04- variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## if we assign a name to a set of data, then that is variable
+# SYNTAX: VAR=DATA
+
+# Number
+d=100
+# String
+b=abc
+
+# in bash shell and also by default there are no data types, Shell considers everything as string
+# Access the data in shell using 3 character prefixing the variable name
+
+echo value of a = $a
+echo value of b = $b
