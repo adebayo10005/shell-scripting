765c4876e97e0a6a07b477504625fb921ee61598
BOKO <adebayo10005@gmail.com>
2021-11-04 19:32:06 -0700

    Added topic terraform

--- terraform/waka.tf
+++ terraform/waka.tf
@@ -1,5 +1,5 @@
-provider "aws"{
-        region = us-east-1
+provider "aws" {
+        region  = "us-east-1"
 }
 
 resource "aws_instance" "smart" {
@@ -24,16 +24,25 @@ resource "aws_security_group" "allow_smart" {
                         protocol         = "tcp"
                         cidr_blocks      = ["0.0.0.0/0"]
                         ipv6_cidr_blocks = []
+                        prefix_list_ids  = []
+                        security_groups  = []
+                        self             = false
+
                 }
         ]
 
         egress = [
                 {
+                        description      = [egress]
                         from_port        = 0
                         to_port          = 0
                         protocol         = "-1"
                         cidr_blocks      = ["0.0.0.0/0"]
                         ipv6_cidr_blocks = ["::/0"]
+                        prefix_list_ids  = []
+                        security_groups  = []
+                        self             = false
+
                 }
         ]
 
