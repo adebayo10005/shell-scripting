d76dfe5dd85e843f0bf72cb2c5d2bf39757df94a
BOKO <adebayo10005@gmail.com>
2021-11-04 17:57:19 -0700

    Added topic terraform

---
+++ terraform.tf/waka.tf
@@ -0,0 +1,18 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-0dc863062bc04e1de"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
