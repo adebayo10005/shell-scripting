851dd96eac26a63176f65c13fb694325e0d92b63
adebayo <ahmedadeba345@outlook.com>
2022-09-15 16:39:37 -0400

    Update frontend

--- Roboshop Project/Component/frontend.sh
+++ Roboshop Project/Component/frontend.sh
@@ -1,13 +1,9 @@
 yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
 Curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-master static README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv frontend-main/static/* .
+mv front-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
 systemctl restart nginx
